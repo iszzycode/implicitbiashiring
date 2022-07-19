73eec1a6f524577fbdabafff164244e77f888191
iszzycode <109562246+iszzycode@users.noreply.github.com>
2022-07-19 12:31:21 +1000

    fingers crossed it all works

--- IATWork.js
+++ IATWork.js
@@ -3,43 +3,43 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 
 	return iatExtension({
 		category1 : {
-			name : 'ATSI people', //Will appear in the data.
+			name : 'Aboriginal and Torres Strait Islander Peoples', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : 'Aboriginal and Torres Strait Islander Peoples'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    {image : 'ATSI face 1.jpg'}, 
+    			{image : 'ATSI face 2.jpg'}, 
+    			{image : 'ATSI face 3.jpg'}, 
+    			{image : 'ATSI face 7.jpg'}, 
+    			{image : 'ATSI face 8.jpg'}, 
+    			{image : 'ATSI face 9.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'Anglo people', //Will appear in the data.
+			name : 'Anglo-Australian people', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {word : 'Anglo-Australian people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    		    {image : 'anglo face 4.jpg'}, 
+    			{image : 'anglo face 5.jpg'}, 
+    			{image : 'anglo face 6.jpg'}, 
+    			{image : 'Anglo face 10.jpg'}, 
+    			{image : 'Anglo face 11.jpg'}, 
+    			{image : 'anglo face 12.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
+			image : 'https://iszzycode.github.io/implicitbiashiring/images/'
 		} 
 	});
 });
